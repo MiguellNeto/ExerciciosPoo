e578e3a96f1831e4cac96bae3c705b7245a41bdb
MiguellNeto <56120625+MiguellNeto@users.noreply.github.com>
2020-02-23 22:31:18 -0300

    ProjetoCilindro
    Desenvolva,usandoaUML(diagramadeclasse),umaclassequemodeleumobjetocilindroem conformidade com o paradigma orientado a objeto. Posteriormente implemente esta classe. A classe deverá ter as seguintes características: raio, altura e cálculo da área lateral, área total e volume. Obs.: os valores do raio e da altura não podem ser negativos. O cálculo da área lateral é (2*3.1415*raio*altura), área total (2*3.1415*raio*(altura+raio)) e volume (3.1415*raio*raio*altura).

---
+++ ProjetoCilindro/src/classes/Cilindro.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author miguelneto
+ */
+public class Cilindro {
+    //Atributos
+    
+    private float raio = 0;
+    private float altura = 0 ;
+    
+    
+    
+    //Metodos
+
+    /**
+     * @return the raio
+     */
+    public float getRaio() {
+        return raio;
+    }
+
+    /**
+     * @param raio the raio to set
+     */
+    public void setRaio(float raio)throws Exception{
+        if(raio < 0 )throw new Exception("Raio não pode ser negativo");
+        this.raio = raio;
+    }
+
+    /**
+     * @return the altura
+     */
+    public float getAltura() {
+        return altura;
+    }
+
+    /**
+     * @param altura the altura to set
+     */
+    public void setAltura(float altura)throws Exception{
+        if(altura < 0 )throw new Exception("Altura não pode ser negativo");
+        this.altura = altura;
+    }
+    
+    
+    
+    public float CalcularAreaLateral(){
+        return (float) (2*3.1415*raio*altura);
+        
+        
+    }
+    public float CalcularAreaTotal(){
+        return (float) ((2*3.1415*raio*(altura+raio)));
+        
+        
+    }
+    public float CalcularVolume(){
+        return (float) (3.1415*raio*raio*altura);
+        
+        
+    }
+    
+}
